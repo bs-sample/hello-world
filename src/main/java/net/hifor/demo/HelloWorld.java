7461e487f058339294d47d7cd7dbf0ea9d1a3edd
ikin <ikin@hifor.net>
2023-09-23 21:04:19 +0800

    init

---
+++ src/main/java/net/hifor/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.hifor.demo;
+
+/**
+ * @author IKin <br/>
+ * @description hello world <br/>
+ * @create 2023/9/23 20:49 <br/>
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
